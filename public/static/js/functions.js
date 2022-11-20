2b5db0985176a786aceb21193a8affc46de757eb
Jiří Lopatář <github@lopatar.me>
2022-11-20 18:55:37 +0100

    Add auto URL shortening

--- public/static/js/functions.js
+++ public/static/js/functions.js
@@ -47,8 +47,35 @@ function savePaste() {
             return;
         }
 
-        window.location.href = data.data.url + keyHex;
+        const url = data.data.url + keyHex;
+        const shortenUrl = document.getElementById('shortenUrl').checked;
+
+        if (shortenUrl) {
+            const shortenedUrl = shortenUrl(url);
+            navigator.clipboard.writeText(shortenedUrl).then(() => {
+                alert('Shortened link copied to clipboard!');
+            });
+        }
+
+        window.location.href = url;
     })
+}
+
+function shortenUrl(url)
+{
+    fetch('https://s.lopatar.me/api/shorten', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            'link': url
+        })
+    }).then(response => response.text()).then(data => {
+        return data;
+    })
+
+    return '';
 }
 
 function decryptPaste() {
